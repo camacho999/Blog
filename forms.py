0ae34a82c79eb0bfdf67b5634d73beaa5242d316
Angel Camacho <yumel@protonmail.com>
2022-12-13 18:39:52 -0600

    se agregan formularios con flaskWFT

--- forms.py
+++ forms.py
@@ -1,11 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length
 
 class SignupForm(FlaskForm):
     name = StringField('Nombre', validators=[DataRequired(), Length(max=64)])
-    Password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Registrar')
 
-    
+class PostForm(FlaskForm):
+    title = StringField('Titulo', validators=[DataRequired(), Length(max=128)])
+    title_slug = StringField('Titulo Slug', validators=[Length(max=128)])
+    content = TextAreaField('Contenido')
+    submit = SubmitField('Enviar')
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, url_for, redirect
-from forms import SignupForm
+from forms import SignupForm, PostForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mikey'
@@ -8,9 +8,7 @@ posts = []
 
 @app.route('/')
 def index():
-    page = request.args.get('page', 1)
-    list = request.args.get('list', 20)
-    return render_template('index.html', num_post=len(posts))
+    return render_template('index.html', posts = posts)
 
 @app.route('/p/<string:slug>/')
 def show_posts(slug):
@@ -18,13 +16,24 @@ def show_posts(slug):
 
 @app.route('/admin/post/')
 @app.route('/admin/post/<int:post_id>/')
-def post_form(post_id=None):
-    return render_template('admin/post_form.html', post_id=post_id)
+def post_form(post_id):
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        title_slug = form.title_slug.data
+        content = form.content.data 
+
+        post = {'title': title, 'title_slug': title_slug, 'content': content}
+        posts.append(post)
+
+        return redirect(url_for('index'))
+    return render_template('admin/post_form.html', form = form)
+
 
 @app.route('/signup/', methods=['GET', 'POST'])
 def show_signup_form():
     form = SignupForm()
-    if request.method == 'POST':
+    if form.validate_on_submit():
         name = request.form['name']
         email = request.form['email']
         password = request.form['password']
@@ -33,4 +42,4 @@ def show_signup_form():
         if next:
             return redirect(next)
         return redirect(url_for('index'))
-    return render_template("signup_form.html")
+    return render_template("signup_form.html", form = form)
