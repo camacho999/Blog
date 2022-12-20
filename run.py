55a973064144a74774db9c81c2d9428f0469e34a
Angel Camacho <yumel@protonmail.com>
2022-12-19 18:47:51 -0600

    se agrega login de usuario

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, url_for, redirect
-from forms import SignupForm, PostForm
+from forms import SignupForm, PostForm, LoginForm
+from flask_login import LoginManager, current_user, login_user, logout_user
+from models import users, get_user, User
+from werkzeug.urls import url_parse
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mikey'
+login_manager = LoginManager(app)
 
 posts = []
 
@@ -32,14 +36,60 @@ def post_form(post_id):
 
 @app.route('/signup/', methods=['GET', 'POST'])
 def show_signup_form():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = SignupForm()
     if form.validate_on_submit():
-        name = request.form['name']
-        email = request.form['email']
-        password = request.form['password']
+        name = form.name.data
+        email = form.email.data
+        password = form.password.data
+        #crea usuario y se guardan
+        user = User(len(users) + 1, name, email, password)
+        users.append(user)
+        # se deja usuario logeado
+        login_user(user, remember = True)
+        next_page = request.args.get('next', None)
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('signup_form.html', form = form)
 
-        next = request.args.get('next',None)
-        if next:
-            return redirect(next)
+    # form = SignupForm()
+    # if form.validate_on_submit():
+    #     name = request.form['name']
+    #     email = request.form['email']
+    #     password = request.form['password']
+
+    #     next = request.args.get('next',None)
+    #     if next:
+    #         return redirect(next)
+    #     return redirect(url_for('index'))
+    # return render_template("signup_form.html", form = form)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if current_user.is_autenticated:
         return redirect(url_for('index'))
-    return render_template("signup_form.html", form = form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = get_user(form.email.data)
+        if user is not None and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            next_page = request.args.get('next')
+            if not next_page or url_parse(next_page).netloc !='':
+                next_page = url_for('index')
+    return render_template('login_form.html', form = form)
+
+@app.route('logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+    
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    for user in users:
+        if user.id == int(user_id):
+            return user
+    return None        
